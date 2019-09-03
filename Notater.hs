f4550e47da86e872e9c485bc3f50bb9bc8f2c16d
S571736 <Sondre.Gjesdal@gmail.com>
2019-09-03 08:48:14 +0200

    FIrst

---
+++ Notater.hs
@@ -0,0 +1,31 @@
+-- 03.09.2019
+
+-- Nytt fra gammelt
+splitAt :: Int -> [a] -> ([a], [a])
+splitAt n xs = (take n xs, drop n xs)
+
+-- betingede likninger
+og x y = if x==False the False
+    else if y==False then False
+        else True
+
+-- voktede likninger
+og x y | x==False = False
+        | y==Fasle = False
+        | otherwise = True
+
+-- Anonyme funksjoner
+
+\ x -> \ y -> if x==False then False else y
+
+-- Mønstre
+og False _ = False
+og _ False = False
+og _ _ = True
+
+og True True = True
+og _ _ = False
+
+og False _ = False
+og True x = x
+
