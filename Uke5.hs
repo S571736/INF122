8d0227be24d702e61c7b1c6d800db9338805b2db
S571736 <Sondre.Gjesdal@gmail.com>
2019-09-23 11:25:25 +0200

    Uke 5 done

--- Uke5.hs
+++ Uke5.hs
@@ -31,3 +31,23 @@ dec2int = foldl leggTil 0
     where leggTil num d = 10*num + d
 
 --7.5
+curry f a b = f(a,b)
+
+uncurry f(a,b)= f a b 
+
+--7.9
+altMap :: (a -> b) -> (a -> b) -> [a] -> [b]
+altMap _ _ [] = []
+altMap f g (x: xs) = f x : altMap g f xs
+
+--7.6
+unfold p h t x 
+    | p x = []
+    | otherwise = h x : unfold p h t (t x)
+
+
+chop8' = unfold null (take 8) (drop 8)
+
+map' f = unfold null (f . head) tail
+
+iterate' f = unfold (const False) id f  
