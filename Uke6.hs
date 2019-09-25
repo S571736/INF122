11c1e09284e932e596e2a550400ae6466b4fc9f5
S571736 <Sondre.Gjesdal@gmail.com>
2019-09-25 18:30:29 +0200

    Uke 6 ferdig

--- Uke6.hs
+++ Uke6.hs
@@ -18,14 +18,35 @@ data Expr = Val Int | Add Expr Expr
 
 folde :: (Int -> a) -> (a -> a -> a) -> Expr -> a
 
-folde f g e = case e of
-    Val x -> f x
-    Add x y -> g (folde f g x) (folde f g y)
-
-
+folde f g (Val x) = f x
+folde f g (Add x y) = g (folde f g x) (folde f g y)
 --8.6
 eval :: Expr -> Int
-eval e = folde (\x -> x) (\x y -> x+y) e
+eval e = folde id (+) e
 
 size :: Expr -> Int
-size e = 
+size e = folde (\_ -> 1) (+) e
+
+--A
+data Exp = S | U | Og Exp Exp | El Exp Exp | Ik Exp
+
+--a
+folde' :: a-> a -> (a -> a -> a) -> (a -> a -> a) -> (a -> a) -> Exp -> a
+folde' s u o e i S = s
+folde' s u o e i U = u
+folde' s u o e i (Og x y) = o (folde' s u o e i x) (folde' s u o e i y)
+folde' s u o e i (El x y) = e (folde' s u o e i x) (folde' s u o e i y)
+folde' s u o e i (Ik x) = i (folde' s u o e i x)
+
+--b
+evb :: Exp -> Bool
+evb exp = folde' True False (&&) (||) (not) exp
+
+--c 
+succMax x y = succ(max x y)
+evh :: Exp -> Int
+evh exp = folde' 1 1 succMax succMax succ exp
+
+--d
+evi :: Exp -> Int
+evi exp = folde' 1 5 (+) (*) negate exp
