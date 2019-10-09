88d5bd1bfcdcd2b6e70e8d82aeb92936485b10a5
S571736 <Sondre.Gjesdal@gmail.com>
2019-10-09 14:13:47 +0200

    o2

--- Oblig2.hs
+++ Oblig2.hs
@@ -1,15 +1,33 @@
--- ditt Navn og Etternavn 
+-- Sondre Lindaas Gjesdal
 
 module Oblig2 where
 
 import Data.Char
 
-data Ast = Tall Int | Sum Ast Ast | Var String  deriving (Eq, Show)
+data Ast = Tall Int | Sum Ast Ast | Mult Ast Ast | Min Ast |  Var String   deriving (Eq, Show)
 -- denne definisjonen av Ast utvides med manglende bitene for Mult og Min 
 
-parse :: String -> Ast
+--parse :: String -> Ast
 
-parse str = undefined
+parse str = parseE [str]
+
+onlyDigits x = takeWhile isDigit x == x
+
+parseE :: [String] -> (Ast, [String])
+
+parseE ("+":rs) = let(e1, r1) = parseE rs ;
+                     (e2, r2) = parseE r1 in (Sum e1 e2, r2)
+
+parseE ("*":rs) = let(e1, r1) = parseE rs ;
+                     (e2, r2) = parseE r1 in (Mult e1 e2, r2)
+            
+parseE ("-":rs) = let(e1, r1) = parseE r1 in (Min e1, r1)
+
+parseE (x:rs) = if (onlyDigits x) then (Tall (read x :: Int), rs)
+                        else error ("Syntaksfeil ved " ++ x)
+parseE ([]:rs) = []
+                        
+
 
 viss :: Ast -> String
 
@@ -34,3 +52,5 @@ evix str i = undefined
 evbx :: Ast -> Int -> Bool
 
 evbx str i = undefined
+
+
