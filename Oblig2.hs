9cffa8e55254f370b530c0fbe5189bf213659a36
Sondre <Sondre.Gjesdal@gmail.com>
2020-11-11 14:47:56 +0100

    Making great progress compared to last year

--- Oblig2.hs
+++ Oblig2.hs
@@ -1,28 +1,59 @@
 -- Lage "main" metode å referere til fra den ekte main så man kan få med nødvendig data
+-- 
 
 import Data.List
-main = do 
-    putStrLn ("Vennligst skriv en kommando")
-    ord <- getLine
-    let input = words ord
-    if (null input) then return () else case (head input) of
-        "c" -> do c (input !! 1) 
-        "n" -> do n (tail input) -- Hjelpefunksjon for å lage liste av tupler?
-        "e" -> do e (tail input)
-        "b" -> do b (input !! 1) (input !! 2)
-        "s" -> do s (input !! 1) (input !! 2)
-        "?" -> help
-        "l" -> do l (input !! 1)
-        "w" -> do w
-        "q" -> do quit
-        otherwise -> do {putStrLn "Ukjent kommando"}
+
+menu :: Matrise -> Brett -> IO ()
+menu matrise brett = do
+  brett matrise
+  putStrLn ("Vennligst skriv en kommando")
+  ord <- getLine
+  let input = words ord
+  if (null input)
+    then return ()
+    else case (head input) of
+      "c" -> do c (read $ input !! 1)
+      "n" -> do n (map (read :: String -> Int) $ tail input) matrise brett -- Hjelpefunksjon for å lage liste av tupler?
+      "e" -> do e (map (read :: String -> Int) $ tail input) matrise brett
+      "b" -> do b (input !! 1) (input !! 2)
+      "s" -> do s (input !! 1) (input !! 2)
+      "?" -> help
+      "l" -> do l (input !! 1)
+      "w" -> do w
+      "q" -> do quit
+      otherwise -> do putStrLn "Ukjent kommando"
+  goto (0, (length matrise) + 2)
+  putStrLn "\ESC[0J"
+  return ()
+
+c n = do 
+  let m = nyMatrise n
+  menu m []
 
 
-c n = undefined
+n :: [Int] -> [[Char]] -> Brett -> IO ()
+n (x : xx : xs) m brett = do
+  if ((length (x : xx : xs)) `mod` 2 == 0)
+    then do
+      let matrise = updateMat (x, xx) m 'O'
+      if (xs /= [])
+        then do
+          n xs matrise brett
+          return ()
+        else menu matrise brett
+    else error "List is not even, list needs to be even to work"
 
-n (x:xs) = undefined
 
-e (x:xs) = undefined
+e :: [Int] -> [[Char]] -> Brett -> IO ()
+e (x : xx : xs) m brett = do
+  if ((length (x : xx : xs)) `mod` 2 == 0)
+    then do
+      let matrise = updateMat (x, xx) m '.'
+      if (xs /= [])
+        then do
+          n xs matrise brett
+          else menu matrise brett
+    else error "List is not even, list needs to be even to work"
 
 b m n = undefined
 
@@ -36,13 +67,65 @@ r name = undefined
 
 l x = undefined
 
-quit = undefined
-
+quit = return ()
 
 -- HJELPEFUNKSJONER --
 
-goto (x,y) = putStr ("\ESC[" ++ show y ++";"++show x ++ "H")
+type Matrise = [[Char]] -- Alle tegnene i matrisen
+
+type Pos = (Int, Int) -- Posisjon
+
+type Brett = [Pos] -- Liste med alle levende posisjoner
+
+brett :: [[Char]] -> IO ()
+brett [[]] = putStrLn ""
+brett [] = putStrLn ""
+brett m = do
+  let n = length m
+  clr
+  putStr "   "
+  goto (lft + 1, 0)
+  printK (map show [1 .. n])
+  lagBrett [1 .. n] m
+
+
+lagBrett [] _ = return ()
+lagBrett (x : xs) (y:ys) = do
+  lagLinje x (y)
+  lagBrett xs (ys)
+
+
+lagLinje rad x = do
+  goto ((if rad > 9 then (lft - 2) else lft - 1, 1 + rad))
+  putStr (show rad)
+  putStr " "
+  putStrLn $ id $ intersperse ' ' $ intersperse ' ' x
+  
+  
+
+
+nyMatrise n = [['.' | a <- [1..n]]| b <- [1..n]]
+
+goto (x, y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+printK [] = return ()
+printK (x : xs) = do
+  if (read x :: Integer) < 9
+    then putStr (x ++ "  ")
+    else putStr (x ++ " ")
+  printK xs
 
 -- clear the screen
 clr :: IO ()
 clr = putStr "\ESC[2J"
+
+lft :: Int
+lft = 3
+
+updateMat (x, y) matrise tegn = do
+  take x matrise
+    ++ [ take y (matrise !! x)
+           ++ [tegn]
+           ++ drop (y + 1) (matrise !! x)
+       ]
+    ++ drop (x + 1) matrise
